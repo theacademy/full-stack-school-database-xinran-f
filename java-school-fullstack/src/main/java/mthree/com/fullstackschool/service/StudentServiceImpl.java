db1ebd7c610492a16480ab2afa7e6c5889c8b4e5
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-19 13:26:52 -0400

    Commit 6: Implement StudentServiceImpl (incomplete)

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/StudentServiceImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/service/StudentServiceImpl.java
@@ -12,61 +12,89 @@ import java.util.List;
 public class StudentServiceImpl implements StudentServiceInterface {
 
     //YOUR CODE STARTS HERE
+    private StudentDao studentDao;
 
-
+    public StudentServiceImpl(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
     //YOUR CODE ENDS HERE
 
     public List<Student> getAllStudents() {
         //YOUR CODE STARTS HERE
-
-        return null;
-
+        return studentDao.getAllStudents();
         //YOUR CODE ENDS HERE
     }
 
     public Student getStudentById(int id) {
         //YOUR CODE STARTS HERE
+        Student student;
 
-        return null;
+        try {
+            student = studentDao.findStudentById(id);
+        } catch (DataAccessException e) {
+            student = new Student();
+            student.setStudentFirstName("Student Not Found");
+            student.setStudentLastName("Student Not Found");
+        }
 
+        return student;
         //YOUR CODE ENDS HERE
     }
 
     public Student addNewStudent(Student student) {
         //YOUR CODE STARTS HERE
+        if (student.getStudentFirstName() == null || student.getStudentFirstName().trim().isEmpty()) {
+            student.setStudentFirstName("First Name blank, student NOT added");
+        }
 
-        return null;
+        if (student.getStudentLastName() == null || student.getStudentLastName().trim().isEmpty()) {
+            student.setStudentLastName("Last Name blank, student NOT added");
+        }
 
+        return studentDao.createNewStudent(student);
         //YOUR CODE ENDS HERE
     }
 
     public Student updateStudentData(int id, Student student) {
         //YOUR CODE STARTS HERE
+        if (id != student.getStudentId()) {
+            student.setStudentFirstName("IDs do not match, student not updated");
+            student.setStudentLastName("IDs do not match, student not updated");
+        }
 
-        return null;
+        studentDao.updateStudent(student);
 
+        return student;
         //YOUR CODE ENDS HERE
     }
 
     public void deleteStudentById(int id) {
         //YOUR CODE STARTS HERE
-
-
-
+        studentDao.deleteStudent(id);
         //YOUR CODE ENDS HERE
     }
 
+    // TODO
+    // If student first name is equal to "Student Not Found", print a line to server console: "Student not found"
+    // else if the course name is equal to "Course Not Found", print a line to server console: "Course not found"
+    // else print a line to server console: "Student: <STUDENT ID> deleted from course: <COURSE ID>"
     public void deleteStudentFromCourse(int studentId, int courseId) {
         //YOUR CODE STARTS HERE
 
-
-
+        studentDao.deleteStudentFromCourse(studentId, courseId);
+        
         //YOUR CODE ENDS HERE
     }
 
+    // TODO
+    // If student first name is equal to "Student Not Found", print a line to the server console "Student not found"
+    // else if the course name is equal to "Course Not Found", print a line to the server console "Course not found"
+    // else print a line to the server console: "Student: <STUDENT ID> added to course: <COURSE ID>"
+    // or catch exception print line to server console: "Student: <STUDENT ID> already enrolled in course: <COURSE ID>"
     public void addStudentToCourse(int studentId, int courseId) {
         //YOUR CODE STARTS HERE
 
+        studentDao.addStudentToCourse(studentId, courseId);
 
         //YOUR CODE ENDS HERE
     }
