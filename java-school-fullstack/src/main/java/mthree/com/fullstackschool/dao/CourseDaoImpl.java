f1f5cbcde87112b746750539495516e4a4ce88e5
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-19 13:44:45 -0400

    Commit 8: Implement CourseDaoImpl (incomplete)

--- java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/CourseDaoImpl.java
+++ java-school-fullstack/src/main/java/mthree/com/fullstackschool/dao/CourseDaoImpl.java
@@ -5,6 +5,8 @@ import mthree.com.fullstackschool.model.Course;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
@@ -21,54 +23,76 @@ public class CourseDaoImpl implements CourseDao {
     @Override
     public Course createNewCourse(Course course) {
         //YOUR CODE STARTS HERE
+        final String sql = "INSERT INTO course(courseCode, CourseDesc, TeacherId) VALUES(?,?,?);";
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
 
+        jdbcTemplate.update((Connection conn) -> {
+            PreparedStatement statement = conn.prepareStatement(
+                    sql,
+                    Statement.RETURN_GENERATED_KEYS);
 
-        return null;
+            statement.setString(1, String.valueOf(course.getCourseId()));
+            statement.setString(2, course.getCourseDesc());
+            statement.setString(3, String.valueOf(course.getTeacherId()));
+            return statement;
 
+        }, keyHolder);
+
+        course.setCourseId(keyHolder.getKey().intValue());
+
+        return course;
         //YOUR CODE ENDS HERE
     }
 
     @Override
     public List<Course> getAllCourses() {
         //YOUR CODE STARTS HERE
-
-
-        return null;
-
+        final String sql = "SELECT cid, courseCode, CourseDesc, teacherId;";
+        return jdbcTemplate.query(sql, new CourseMapper());
         //YOUR CODE ENDS HERE
     }
 
     @Override
     public Course findCourseById(int id) {
         //YOUR CODE STARTS HERE
+        final String sql = "SELECT cid, courseCode, courseDesc, teacherId "
+                + "FROM course WHERE cid = ?;";
 
-        return null;
-
+        return jdbcTemplate.queryForObject(sql, new CourseMapper(), id);
         //YOUR CODE ENDS HERE
     }
 
     @Override
     public void updateCourse(Course course) {
         //YOUR CODE STARTS HERE
+        final String sql = "UPDATE course SET "
+                + "courseCode = ?, "
+                + "courseDesc = ?, "
+                + "teacherId = ? "
+                + "WHERE cid = ?;";
 
-
-
+        jdbcTemplate.update(sql,
+            course.getCourseName(),
+            course.getCourseDesc(),
+            course.getTeacherId(),
+            course.getCourseId());
         //YOUR CODE ENDS HERE
     }
 
     @Override
     public void deleteCourse(int id) {
         //YOUR CODE STARTS HERE
+        final String sql = "DELETE FROM course WHERE cid = ?;";
 
-
-
+        jdbcTemplate.update(sql, id);
         //YOUR CODE ENDS HERE
     }
 
+
+    // TODO
     @Override
     public void deleteAllStudentsFromCourse(int courseId) {
         //YOUR CODE STARTS HERE
-
 
 
         //YOUR CODE ENDS HERE
